ccd828572c3b9d30bf95e3919467beb4b1268768
klau2000 <klau00@stuy.edu>
2020-01-05 15:27:47 -0500

    again?

--- gameplay.c
+++ gameplay.c
@@ -15,7 +15,7 @@ void printBoard(struct cell gameBoard[ROWS][COLS]){
   int i = 0, j = 0;
   printf("  0 1 2 3 4 5 6 7 8 9\n");
   for (i = 0; i < ROWS; i++) {
-    printf("%c ", i);
+    printf("%c ", gameBoard[i][j].position.row);
     for (j = 0; j < COLS; j++) {
       switch(gameBoard[i][j].symbol){
         case HIT: printf("%c ", HIT); break;
