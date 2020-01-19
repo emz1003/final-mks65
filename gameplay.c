77c4a1cddd5ef020e7b81a4e7c60a642cfa047f7
klau2000 <klau00@stuy.edu>
2020-01-19 15:07:06 -0500

    again

--- gameplay.c
+++ gameplay.c
@@ -192,7 +192,7 @@ void playerOne(){
     fd = open(playerMove, O_RDONLY);
     while(1){
       if(read(fd, hit2, sizeof(hit2)) >0 ){
-          strchr(hit2, '\n');
+          *strchr(hit2, "\n") = '\0';
           printf("Player Two: %s\n", hit2);
           break;
       }
@@ -209,7 +209,7 @@ void playerTwo(){
     fd2 = open(playerMove, O_RDONLY);
     while(1){
       if(read(fd2, hit1, sizeof(hit1)) >0){
-          strchr(hit1, '\n');
+          *strchr(hit1, "\n") = '\0';
           printf("Player One: %s\n", hit1);
           break;
       }
